301f6bb394e63337faf44f0ad30a00de7ad83c2c
Elizabeth Wells <elizabethannwells1@gmail.com>
2018-03-26 23:48:11 -0400

    Added words in common and took out words in between
    Up to 19% F1

--- Relation Extraction/feature_extractor.py
+++ Relation Extraction/feature_extractor.py
@@ -1,5 +1,6 @@
 import os
 import re
+from nltk.corpus import gazetteers
 
 postagged_path = './data/postagged-files'
 parsed_path = './data/parsed-files'
@@ -47,9 +48,10 @@ def open_context_file(file, mentions):
         lines = raw_tagged_file.readlines()
         replace_regex = re.compile('()')
         cleaned_lines = [replace_regex.sub('', line) for line in lines]
+        # print(cleaned_lines[1])
 
         inbetween_counter = 0
-        inbetween_words = ''
+        # inbetween_words = ''
 
         mention_found = False
         word_features = {}
@@ -63,17 +65,17 @@ def open_context_file(file, mentions):
                 if mention_found:
                     if word.split('_')[0] == mentions[1]:
                         word_features = get_context(index, line.split(), word_features, 'arg2')
-                        word_features['inbetween_context-words'] = inbetween_words
+                        # word_features['inbetween_context-words'] = inbetween_words
                         word_features['inbetween_context-distance'] = inbetween_counter
 
                         return word_features
 
                     inbetween_counter += 1
-                    inbetween_words += ' ' + word.split('_')[0]
+                    # inbetween_words += ' ' + word.split('_')[0]
 
     word_features['arg2_context-1'] = -1
     word_features['arg2_context+1'] = -1
-    word_features['inbetween_context-words'] = inbetween_words
+    # word_features['inbetween_context-words'] = inbetween_words
     word_features['inbetween_context-distance'] = inbetween_counter
 
     return word_features
@@ -131,17 +133,21 @@ def write_to_file(path, gold_file, train=True):
     file_out = open(path, 'w')
     relations, words, types, word_features = read_train_gold(gold_file)
     pos = read_pos_files(postagged_path)
-    print(pos)
+    locations = gazetteers.words()
+    # print(pos)
     for x in range(len(relations)):
         arg1,arg2 = words[x][0], words[x][1]
         arg1_type,arg2_type = types[x][0], types[x][1]
         arg1_pos, arg2_pos = pos[rel_to_tokenized(arg1)], pos[rel_to_tokenized(arg2)]
+        # arg1_gaz, arg2_gaz = arg1.replace('_', ' ') in locations, arg2.replace('_', ' ') in locations
+        common_words = len(set(arg1.split('_')).intersection(set(arg2.split('_')))) > 0
         if train:
             file_out.write(relations[x]+" ")
         file_out.write("arg1=" + arg1 + " " + "arg2=" + arg2 + " ")
         file_out.write("arg1_type=" + arg1_type + " " + "arg2_type=" + arg2_type + " ")
         file_out.write("arg1_pos=" + arg1_pos + " " + "arg2_pos=" + arg2_pos + " ")
-
+        # file_out.write("arg1_gaz=" + str(arg1_gaz) + " " + "arg2_gaz=" + str(arg2_gaz) + " ")
+        file_out.write("common_words=" + str(common_words) + " ")
         for key in word_features[x]:
             if key == 'inbetween_context-distance':
                 file_out.write(key + '=' + str(word_features[x][key]) +  ' ')
